901a333d5c1ee968228cb618044cbc2631fc4231
MariaPaulaArdilam <antonietainfo@gmail.com>
2023-10-17 11:21:50 -0500

    se implemento SPA

--- src/main.js
+++ src/main.js
@@ -3,3 +3,43 @@
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+
+import {login} from './view.js';
+
+const routes = [
+  { path: '/', component: login },
+  { path: '/login', component: login },
+//   { path: '/error', component: error },
+];
+
+const defaultRoute = '/';
+const root = document.getElementById('root');
+
+function navigateTo(hash) {
+  const route = routes.find((routeFound) => routeFound.path === hash);
+  
+  if (route && route.component) {
+    window.history.pushState(
+      {},
+      route.path,
+      window.location.origin + route.path,
+    );
+
+    if (root.firstChild) {
+      root.removeChild(root.firstChild);
+    }
+
+    root.appendChild(route.component(navigateTo));
+   } else {navigateTo('/');}
+    
+
+}
+  
+
+
+window.onpopstate = () => {
+  navigateTo(window.location.pathname);
+};
+
+navigateTo(window.location.pathname);
