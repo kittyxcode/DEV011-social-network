f1bed75ee8081efbaf36a9c72170b06a6da7522e
MariaPaulaArdilam <antonietainfo@gmail.com>
2023-10-20 11:08:06 -0500

    se puede crear cuenta y envia correo de verificacion y login con cuenta verificada

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,16 @@
 // aqui exportaras las funciones que necesites
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import {signInWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
 import { auth } from '../firebase';
 
 export const crearUsuarioConCorreoYContrasena = (email, password) => {
   return createUserWithEmailAndPassword(auth, email, password);
 };
+
+export const ingresoUsuarioExistente =(email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+export const correoValidacion = () =>{
+  return sendEmailVerification(auth.currentUser)
+}
--- src/views/createAcount.js
+++ src/views/createAcount.js
@@ -1,3 +1,6 @@
+import { auth } from '../firebase';
+import { crearUsuarioConCorreoYContrasena, correoValidacion } from '../lib';
+
 export function createAcount(navigateTo) {
   const section = document.createElement('section');
   const imagen = document.createElement('img');
@@ -7,29 +10,35 @@ export function createAcount(navigateTo) {
   const buttonBack = document.createElement('button');
   const form = document.createElement('form');
   const inputEmail = document.createElement('input');
-  const inputUser = document.createElement('input');
   const inputPass = document.createElement('input');
   const buttonSingUp = document.createElement('button');
 
 
   imagen.src = './img/logo.png';
+  inputPass.type= 'password'
   buttonSingUp.textContent = 'Sing Up';
-  buttonSingUp.addEventListener('click', () => {
-    navigateTo('/home');
-  });
-  buttonBack.textContent ='<-'
-  buttonBack.addEventListener('click', () => {
-    navigateTo('/');
+  buttonSingUp.addEventListener('click', (e) => {
+    e.preventDefault();
+    crearUsuarioConCorreoYContrasena(inputEmail.value, inputPass.value)
+      .then(() => {
+        correoValidacion()
+        alert('Cuenta Creada');
+        navigateTo('/');
+      })
+      .catch((error) => {
+        console.error('Error al crear la cuenta:', error);
+        // Puedes mostrar un mensaje de error al usuario si es necesario.
+      });
 
-  });
+ });
 
   email.textContent = 'Enter Email Id';
   titleUser.textContent = 'Create User Name';
   titlePass.textContent = 'Create Password';
 
-  form.append(inputUser, inputPass, inputEmail, buttonSingUp);
+  form.append(email, inputEmail,titlePass,inputPass, buttonSingUp);
 
-  section.append(imagen,titleUser, titlePass, form, email, buttonBack);
+  section.append(imagen,  form,  buttonBack);
 
   return section;
 }
