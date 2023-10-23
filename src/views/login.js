8d28c0c66c8ac2a4b73127bd51a3c7c03b1998c4
MariaPaulaArdilam <antonietainfo@gmail.com>
2023-10-22 19:47:12 -0500

    se hicieron estilos para login y create account

--- src/views/login.js
+++ src/views/login.js
@@ -1,92 +1,89 @@
-import { auth } from '../firebase';
-import { ingresoUsuarioExistente, iniciarConGoogle } from '../lib';
-
+import { auth } from "../firebase";
+import { ingresoUsuarioExistente, iniciarConGoogle } from "../lib";
 
 export function login(navigateTo) {
-  const section = document.createElement('section');
-  section.className = 'sectionlogin';
-  const imagen = document.createElement('img');
-  imagen.className = 'logo';
-  const buttonCreate = document.createElement('button');
-  const form = document.createElement('form');
-  form.className = 'formlogin';
-  const titleUser = document.createElement('h4');
-  titleUser.className = 'titleuser';
-  const titlePass = document.createElement('h4');
-  titlePass.className = 'tirlepass';
-  const inputUser = document.createElement('input');
-  inputUser.className = 'imputname';
-  const inputPass = document.createElement('input');
-  inputPass.className = 'imputpass';
-  inputPass.type = 'password';
-  const buttonLogin = document.createElement('button');
-  const text = document.createElement('p');
-  const googleLogo = document.createElement('img');
-  googleLogo.className = 'googleImg';
-  const boton =document.createElement('button')
+  const section = document.createElement("section");
+  section.className = "sectionlogin";
+  const imagen = document.createElement("img");
+  imagen.className = "logo";
+  const buttonCreate = document.createElement("p");
+  buttonCreate.className = "createAccount";
+  const form = document.createElement("form");
+  form.className = "formlogin";
+  const titleUser = document.createElement("h4");
+  titleUser.className = "titleuser";
+  const titlePass = document.createElement("h4");
+  titlePass.className = "tirlepass";
+  const inputUser = document.createElement("input");
+  inputUser.className = "imputname";
+  const inputPass = document.createElement("input");
+  inputPass.className = "imputpass";
+  inputPass.type = "password";
+  const buttonLogin = document.createElement("button");
+  const text = document.createElement("p");
+  text.className = "textp";
+  const googleLogo = document.createElement("img");
+  googleLogo.className = "googleImg";
 
-  imagen.src = './img/logo.png';
-  googleLogo.src = './img/google.png';
-  boton.textContent = 'google'
-  boton.addEventListener ('click', (e) =>{
+  imagen.src = "./img/logo.png";
+  googleLogo.src = "./img/google.png";
+  googleLogo.addEventListener("click", (e) => {
     e.preventDefault();
     iniciarConGoogle()
-    .then((result) => {
-      alert('Cuenta Creada');
-      navigateTo('/home');
-    })
-    .catch((error) => {
-      if (error.code) {
-        alert(error);
-      }
-  
- })}
- )
-  
+      .then((result) => {
+        alert("Cuenta Creada");
+        navigateTo("/home");
+      })
+      .catch((error) => {
+        if (error.code) {
+          alert(error);
+        }
+      });
+  });
 
-  buttonLogin.textContent = 'login';
-  buttonCreate.textContent = 'Create a New Account';
-  buttonLogin.addEventListener('click', (e) => {
+  buttonLogin.textContent = "Login";
+  buttonCreate.textContent = "Create a New Account";
+  buttonLogin.addEventListener("click", (e) => {
     e.preventDefault();
-    ingresoUsuarioExistente(inputUser.value, inputPass.value) 
-    .then(() => {
-      alert('Cuenta Creada');
-      navigateTo('/home');
-    })
-    .catch((error) => {
-      if (error.code === "auth/invalid-email") {
-        alert("Your email is not valid");
-      }
-      if (error.code === 'auth/invalid-login-credentials') {
-        alert("Incorrect password");
-      }
-      if (error.code === 'auth/invalid-login-credentials') {
-        alert("Your information is not valid");
-      }
-      if (error.code) {
-        alert(error);
-      }
-  });
-});
-
-auth.onAuthStateChanged(user => {
-  if (user && !user.emailVerified) {
-    console.log("User's email is not verified.");
-  } else {
-    // El usuario ha iniciado sesión y su correo está verificado o no ha iniciado sesión.
-  }
-});
-
-  buttonCreate.addEventListener('click', () => {
-    navigateTo('/createAcount');
+    ingresoUsuarioExistente(inputUser.value, inputPass.value)
+      .then(() => {
+        alert("Cuenta Creada");
+        navigateTo("/home");
+      })
+      .catch((error) => {
+        if (error.code === "auth/invalid-email") {
+          alert("Your email is not valid");
+        }
+        if (error.code === "auth/invalid-login-credentials") {
+          alert("Incorrect password");
+        }
+        if (error.code === "auth/invalid-login-credentials") {
+          alert("Your information is not valid");
+        }
+        if (error.code) {
+          alert(error);
+        }
+      });
   });
 
-  titleUser.textContent = 'Username';
-  titlePass.textContent = 'Password';
-  text.textContent = 'Or login With';
+  auth.onAuthStateChanged((user) => {
+    if (user && !user.emailVerified) {
+      console.log("User's email is not verified.");
+    } else {
+      // El usuario ha iniciado sesión y su correo está verificado o no ha iniciado sesión.
+    }
+  });
+
+  buttonCreate.addEventListener("click", () => {
+    navigateTo("/createAcount");
+  });
+
+  titleUser.textContent = "Username";
+  titlePass.textContent = "Password";
+  text.textContent = "Or login With";
 
   form.append(titleUser, inputUser, titlePass, inputPass, buttonLogin);
-  section.append(imagen, form, buttonLogin, text, buttonCreate, googleLogo, boton);
+  section.append(imagen, form, buttonLogin, text, googleLogo, buttonCreate);
 
   return section;
 }
