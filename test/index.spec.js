13245cc770c8d41b0d927a3e8f1fa15c32e51755
kittyxcode <katherine.pino059@gmail.com>
2023-10-24 17:31:31 -0300

    testing a index.js

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,31 @@
 // importamos la funcion que vamos a testear
-import { crearUsuarioConCorreoYContrasena } from '../src/lib/index';
+import {
+  crearUsuarioConCorreoYContrasena,
+  ingresoUsuarioExistente,
+  correoValidacion,
+  iniciarConGoogle,
+} from '../src/lib/index';
 
-describe('myFunction', () => {
+describe('crear usuario con correo y contrasena', () => {
   it('debería ser una función', () => {
     expect(typeof crearUsuarioConCorreoYContrasena).toBe('function');
+  });
+});
+
+describe('ingreso usuario existente', () => {
+  it('debería ser una función', () => {
+    expect(typeof ingresoUsuarioExistente).toBe('function');
+  });
+});
+
+describe('correo validacion', () => {
+  it('debería ser una función', () => {
+    expect(typeof correoValidacion).toBe('function');
+  });
+});
+
+describe('iniciar con google', () => {
+  it('debería ser una función', () => {
+    expect(typeof iniciarConGoogle).toBe('function');
   });
 });
