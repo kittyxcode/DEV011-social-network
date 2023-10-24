84dd5bf424f2a857e487d97e17124b4716bb01c0
kittyxcode <katherine.pino059@gmail.com>
2023-10-24 16:51:02 -0300

    createAcount.js check

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,27 +1,30 @@
 // aqui exportaras las funciones que necesites
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import {signInWithEmailAndPassword, sendEmailVerification,} from "firebase/auth";
-import { GoogleAuthProvider,signInWithPopup } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  sendEmailVerification,
+} from 'firebase/auth';
 import { auth } from '../firebase';
 
 export const crearUsuarioConCorreoYContrasena = (email, password) => {
-  return createUserWithEmailAndPassword(auth, email, password);
+   return createUserWithEmailAndPassword(auth, email, password);
 };
 
-export const ingresoUsuarioExistente =(email, password) => {
-  return signInWithEmailAndPassword(auth, email, password);
-}
+export const ingresoUsuarioExistente = (email, password) => {
+  signInWithEmailAndPassword(auth, email, password);
+};
 
-export const correoValidacion = () =>{
-  return sendEmailVerification(auth.currentUser)
-}
+export const correoValidacion = () => {
+  return sendEmailVerification(auth.currentUser);
+};
 
-  const provider = new GoogleAuthProvider();
-  export const iniciarConGoogle =() =>{
-   return signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-  }
-    )}
+const provider = new GoogleAuthProvider();
+export const iniciarConGoogle = () => {
+  return signInWithPopup(auth, provider).then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+  });
+};
