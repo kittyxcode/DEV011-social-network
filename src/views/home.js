c518c01c1e50769242d5cafd406ee6c6295393b5
kittyxcode <katherine.pino059@gmail.com>
2023-10-30 14:38:31 -0300

    Poner nombre de usuario en muro

--- src/views/home.js
+++ src/views/home.js
@@ -12,15 +12,24 @@ export function home(navigateTo) {
   const postSection = document.createElement('article');
   postSection.className = 'post-section';
   const nameUser = document.createElement('h2');
-  nameUser.textContent = userAuth;
-  section.append(post, buttonPost, postSection, nameUser);
-
+  const contedorPost = document.createElement('div');
+  contedorPost.id = 'contendorPost';
+  contedorPost.append(nameUser, post, buttonPost);
+  section.append(contedorPost, postSection);
   buttonPost.addEventListener('click', () => {
     const comment = document.querySelector('#inputPost').value;
     console.log('Sirve el click', comment.value);
     createPost(comment);
   });
+  function updateUserDisplayName() {
+    userAuth().then((displayName) => {
+      nameUser.textContent = displayName;
+      // Ahora puedes agregar nameUser al DOM u otro lugar donde desees mostrar el nombre de usuario.
+    });
+  }
 
+  // Llama a updateUserDisplayName para actualizar el nombre de usuario cuando sea necesario.
+  updateUserDisplayName();
   renderPostRealTime((querySnapshot) => {
     post.value = '';
     postSection.textContent = '';
